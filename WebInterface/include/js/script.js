e7b84d8fecc8ae8f7277931dde0934528e4424a7
Octanne <40762269+Octanne@users.noreply.github.com>
2020-02-17 12:36:04 +0100

    add scrollbar

---
+++ WebInterface/include/js/script.js
@@ -0,0 +1,34 @@
+//events (mieux que window.event=function)
+if(window.addEventListener) {
+    window.addEventListener("load",loadPage,false);
+    window.addEventListener("hashchange",loadPage,false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload",loadPage);
+    window.attachEvent("onhashchange",loadPage);
+}
+
+
+
+function loadPage() {
+    var elementFocus = window.location.href;
+    elementFocus = elementFocus.substring(elementFocus.lastIndexOf('/')+2);
+    var menu_links = document.getElementsByClassName("menu_link");
+    if (elementFocus == "" && menu_links.length >= 1) {
+        elementFocus = menu_links[0].id.substring(menu_links[0].id.lastIndexOf("menu")+4);
+        //si elementFocus n'est pas défini (url sans #Name) alors le premier onglet est selectionné
+    }
+    
+    //change le "style" des élements actuels (sommaire et page)
+    for (let i=0; i < menu_links.length; i++) {
+        menu_links[i].setAttribute("open", menu_links[i].id == "menu"+elementFocus ? "true" : "false");
+    }
+    var pages_content = document.getElementsByClassName("pageContent");
+    for (let i=0; i < pages_content.length; i++) {
+        pages_content[i].setAttribute("open", pages_content[i].id == "page"+elementFocus ? "true" : "false");
+    }
+}
+function openPage(element) {
+    window.open("./#"+element.id.substring((element.id.indexOf('u')+1)),"_self");
+    //l'actualisation va se faire par loadPage lors de l'evenement onhashchange
+}
+
