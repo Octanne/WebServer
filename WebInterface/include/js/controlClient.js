0ccd242f12d293cbf5e5f2edcd7f81b29e4f1971
Octanne <40762269+Octanne@users.noreply.github.com>
2020-02-18 22:37:03 +0100

    Add files via upload

--- WebInterface/include/js/controlClient.js
+++ WebInterface/include/js/controlClient.js
@@ -1,7 +1,7 @@
 var wSocket = new WebSocket('wss://' + location.host + '/wss');
 if (!window.console) { window.console = { log: function() {} }; }
 wSocket.onopen = function(ev)  { console.log(ev);
-	setInterval(sendMessage("status", "nothing"), 2000);
+	launchAutoRefresh();
 };
 wSocket.onerror = function(ev) { console.log(ev); };
 wSocket.onclose = function(ev) { console.log(ev); };
@@ -24,7 +24,32 @@ wSocket.onmessage = function(ev) {
 		console.log("Web : " + webStatus + " | Sync : " + syncStatus);
 		console.log("---------------------------------------------");
 		
-		
+		document.getElementById("temperature").innerHTML = temp+"°C";
+		document.getElementById("cpuUsage").innerHTML = cpuUsage+"%"; document.getElementById("cpuUsage").style.width = cpuUsage+"%";
+		document.getElementById("ramUsage").innerHTML = ramUsage+"%"; document.getElementById("ramUsage").style.width = ramUsage+"%";
+		document.getElementById("netUsageTX").innerHTML = "<div id=\"netUsageRX\" class=\"progress-bar\" style=\"width:0%;\">"+netUsage+"%</div>"+(100-netUsage)+"%";
+		document.getElementById("netUsageRX").style.width = netUsage+"%";
+		if(temp <= 45){
+			document.getElementById("temperature").style.color = "green";
+		}else if(temp <= 50){
+			document.getElementById("temperature").style.color = "darkorange";
+		}else if(temp > 60){
+			document.getElementById("temperature").style.color = "red";
+		}
+		if(syncStatus){
+			document.getElementById("syncStat").style.color = "green";
+			document.getElementById("syncStat").innerHTML = "ACTIVE";
+		}else{
+			document.getElementById("syncStat").style.color = "red";
+			document.getElementById("syncStat").innerHTML = "INACTIF";
+		}
+		if(webStatus){
+			document.getElementById("webStat").style.color = "green";
+			document.getElementById("webStat").innerHTML = "ACTIVE";
+		}else{
+			document.getElementById("webStat").style.color = "red";
+			document.getElementById("webStat").innerHTML = "INACTIF";
+		}
 	}
 	if(webMessage.type == "consoleUP"){
 		
@@ -42,4 +67,8 @@ function sendMessage(type, arg = "nothing"){
 		args: arg
 	};
 	wSocket.send(JSON.stringify(webMessage).toString());
+}
+function launchAutoRefresh(){
+	sendMessage("status");
+	setInterval(sendMessage, 4000, "status", "nothing");
 }
